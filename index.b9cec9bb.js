e6ea9b65ec62495cbeaeb8019f5aece16246a918
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2022-11-20 13:33:04 +0000

    Deploying to gh-pages from @ Maryna-Harasko/parcel-bland17@822ae56eae077394c7b034c3643e0844922315ac 🚀

--- index.b9cec9bb.js
+++
@@ -1,2 +0,0 @@
-window.onscroll=function(){window.screen.width>=768?scrollY<=70?document.getElementById("header").classList.remove("header-scroll"):document.getElementById("header").classList.add("header-scroll"):scrollY<=120?document.getElementById("header").classList.remove("header-scroll"):document.getElementById("header").classList.add("header-scroll")};
-//# sourceMappingURL=index.b9cec9bb.js.map
